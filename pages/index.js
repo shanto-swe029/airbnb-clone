7b030460e4b55005722b02f180c1c634f38c446f
shanto-swe029 <ariful.shanto786@gmail.com>
2023-08-22 23:07:21 +0600

    implemented the Large Card item

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import Banner from "@/components/Banner";
 import SmallCard from "@/components/SmallCard";
+import MediumCard from "@/components/MediumCard";
+import LargeCard from "@/components/LargeCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +25,7 @@ export default function Home({ exploreData, cardsData }) {
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData.map((item) => (
+            {exploreData?.map((item) => (
               <SmallCard
                 key={item.img}
                 img={item.img}
@@ -36,7 +38,19 @@ export default function Home({ exploreData, cardsData }) {
 
         <section className="pt-6 ">
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map((item) => (
+              <MediumCard key={item.img} img={item.img} title={item.title} />
+            ))}
+          </div>
         </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlist curated by Airbnb."
+          buttonText="Get Inspired"
+        />
       </main>
     </div>
   );
@@ -61,6 +75,7 @@ export async function getStaticProps() {
     return {
       props: {
         exploreData: [], // Provide a default value in case of an error
+        cardsData: [], // Provide a default value in case of an error
       },
     };
   }
